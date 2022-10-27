8baa967741139599b3aa538ff871c519dfbf69e0
saurabh yadav <sy425191@gmail.com>
2022-10-27 14:20:13 +0530

    Node API server commit

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use('/', (req, res)=>{
+    res.send('Netchilzz API endpoint')
+})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
