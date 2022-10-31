1da7a17726179ea68011b6597630268e53f5a26e
saurabh yadav <sy425191@gmail.com>
2022-10-31 16:02:52 +0530

    Media Schema bug fixed

--- api/models/Media.js
+++ api/models/Media.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const MediaSchema = new mongoose.Schema(
   {
-    user : {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    user : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     title: { type: String, required: true },
     description: { type: String },
     imgUrl: { type: String, required: true },
