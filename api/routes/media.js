cf2af8da8d55d31ba095a58925871b159adacc94
saurabh yadav <sy425191@gmail.com>
2022-10-31 12:46:44 +0530

    Your content bug fixed

--- api/routes/media.js
+++ api/routes/media.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Media = require("../models/Media");
 
 router.get("/userAll", verify, async (req, res) => {
-    const user_id = req.user._id;
+    const user_id = req.user.id;
     const uploads = await Media.find({user: user_id});
     res.json(uploads);
 });
