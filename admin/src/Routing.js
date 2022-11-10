fe0a3328e7a45f823d42ff050bbb1e85280953f2
aakashlakhra <chauhanaakash6789@gmail.com>
2022-11-10 10:56:41 +0530

    Admin Pannel All users added

--- admin/src/Routing.js
+++ admin/src/Routing.js
@@ -1,10 +1,14 @@
 import { Routes,Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Users from "./pages/Users";
 
 export default function Routing() {
   return (
     <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
     </Routes>
   )
 }
